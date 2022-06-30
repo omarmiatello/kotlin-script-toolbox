4b68a7a9dcb123aa7a3ee0275579b0f1a8fabc1a
Omar Miatello <jackl85@gmail.com>
2022-06-30 23:03:40 +0200

    Version 0.1.1 - Use OkHttpClient instead of Ktor for Twitter client

--- twitter/src/main/kotlin/com/github/omarmiatello/kotlinscripttoolbox/twitter/TwitterScope.kt
+++ twitter/src/main/kotlin/com/github/omarmiatello/kotlinscripttoolbox/twitter/TwitterScope.kt
@@ -3,9 +3,10 @@ package com.github.omarmiatello.kotlinscripttoolbox.twitter
 import com.github.omarmiatello.kotlinscripttoolbox.core.BaseScope
 import com.github.omarmiatello.kotlinscripttoolbox.core.KotlinScriptToolboxScope
 import com.github.omarmiatello.kotlinscripttoolbox.gson.fromJson
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.request.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okio.ByteString
 import java.net.URLEncoder
 import java.util.*
@@ -13,7 +14,7 @@ import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
 interface TwitterScope : KotlinScriptToolboxScope {
-    val twitterHttpClient: HttpClient
+    val twitterHttpClient: OkHttpClient
 
     suspend fun sendTweet(
         text: String,
@@ -32,15 +33,26 @@ interface TwitterScope : KotlinScriptToolboxScope {
             .map { it.take(MESSAGE_MAX_SIZE) }
             .forEach { msg ->
                 println("🐣 --> $msg")
-                val response: String = twitterHttpClient.post("https://api.twitter.com/2/tweets") {
-                    header("Content-Type", "application/json")
-                    body = TweetMessageRequest(
-                        text = msg,
-                        quote_tweet_id = lastTweetId,
-                    ).toString()
-                }
+
+                val response: String? = twitterHttpClient.newCall(
+                    Request.Builder()
+                        .url("https://api.twitter.com/2/tweets")
+                        .post(
+                            TweetMessageRequest(
+                                text = msg,
+                                quote_tweet_id = lastTweetId,
+                            )
+                                .toString()
+                                .toRequestBody("application/json".toMediaType())
+                        )
+                        .build()
+                )
+                    .execute()
+                    .body
+                    ?.string()
+
                 try {
-                    lastTweetId = response.fromJson<TweetMessageResponse>().id
+                    lastTweetId = response!!.fromJson<TweetMessageResponse>().id
                 } catch (e: Exception) {
                     println("⚠️ Cannot find the tweet id in $response")
                 }
@@ -70,32 +82,30 @@ interface TwitterScope : KotlinScriptToolboxScope {
             accessKey: String,
             accessSecret: String,
         ) = object : TwitterScope {
-            override val twitterHttpClient: HttpClient = HttpClient(OkHttp) {
-                engine {
-                    addInterceptor { chain ->
-                        val request = chain.request()
-                        fun String.encodeUtf8() = URLEncoder.encode(this, "UTF-8").replace("+", "%2B")
-                        val nonce: String = UUID.randomUUID().toString()
-                        val timestamp: Long = System.currentTimeMillis() / 1000L
-                        val urlEncoded = request.url.toString().encodeUtf8()
-                        val paramEncoded =
-                            "oauth_consumer_key=$consumerKey&oauth_nonce=$nonce&oauth_signature_method=HMAC-SHA1&oauth_timestamp=$timestamp&oauth_token=$accessKey&oauth_version=1.0".encodeUtf8()
-                        val signature = Mac.getInstance("HmacSHA1")
-                            .apply { init(SecretKeySpec("$consumerSecret&$accessSecret".toByteArray(), "HmacSHA1")) }
-                            .doFinal("${request.method}&$urlEncoded&$paramEncoded".toByteArray())
-                            .let { ByteString.of(*it) }
-                            .base64()
-                            .encodeUtf8()
+            override val twitterHttpClient = OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val request = chain.request()
+                    fun String.encodeUtf8() = URLEncoder.encode(this, "UTF-8").replace("+", "%2B")
+                    val nonce: String = UUID.randomUUID().toString()
+                    val timestamp: Long = System.currentTimeMillis() / 1000L
+                    val urlEncoded = request.url.toString().encodeUtf8()
+                    val paramEncoded =
+                        "oauth_consumer_key=$consumerKey&oauth_nonce=$nonce&oauth_signature_method=HMAC-SHA1&oauth_timestamp=$timestamp&oauth_token=$accessKey&oauth_version=1.0".encodeUtf8()
+                    val signature = Mac.getInstance("HmacSHA1")
+                        .apply { init(SecretKeySpec("$consumerSecret&$accessSecret".toByteArray(), "HmacSHA1")) }
+                        .doFinal("${request.method}&$urlEncoded&$paramEncoded".toByteArray())
+                        .let { ByteString.of(*it) }
+                        .base64()
+                        .encodeUtf8()
 
-                        chain.proceed(
-                            request.newBuilder().addHeader(
-                                name = "Authorization",
-                                value = """OAuth oauth_consumer_key="$consumerKey", oauth_nonce="$nonce", oauth_signature="$signature", oauth_signature_method="HMAC-SHA1", oauth_timestamp="$timestamp", oauth_token="$accessKey", oauth_version="1.0""""
-                            ).build()
-                        )
-                    }
+                    chain.proceed(
+                        request.newBuilder().addHeader(
+                            name = "Authorization",
+                            value = """OAuth oauth_consumer_key="$consumerKey", oauth_nonce="$nonce", oauth_signature="$signature", oauth_signature_method="HMAC-SHA1", oauth_timestamp="$timestamp", oauth_token="$accessKey", oauth_version="1.0""""
+                        ).build()
+                    )
                 }
-            }
+                .build()
         }
     }
 }
